92e9ce5075b7175ab5eae409a30381b8f9d8c225
dineshbhor <dinesh.bhor@nttdata.com>
2016-09-14 17:31:31 +0530

    Alter nullable constraint on created_at column
    oslo_db takes care of populating 'created_at' column with valid data
    at the time of inserting record so it's guaranteed that 'created_at'
    column will be never null. So there is no need of 'nullable=false'
    for it.
    
    This patch alters the nullable constraint from 'False' to 'True' for
    existing hosts and failover_segments table by adding a new migration
    script.
    
    Closes-Bug: #1623526
    
    Change-Id: I9ce46b43796b790c318e244d722ac0825e36c1cc

---
+++ masakari/db/sqlalchemy/migrate_repo/versions/005_remove_nullable_mismatch.py
@@ -0,0 +1,26 @@
+# Copyright 2016 NTT Data.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from sqlalchemy import MetaData, Table
+
+
+def upgrade(migrate_engine):
+    meta = MetaData(bind=migrate_engine)
+
+    failover_segments = Table('failover_segments', meta, autoload=True)
+    hosts = Table('hosts', meta, autoload=True)
+
+    for table in [failover_segments, hosts]:
+        table.c.created_at.alter(nullable=True)
